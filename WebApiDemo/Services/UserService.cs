6eeb5a6bac834aded0409094715ec96f78987666
surya.arul <surya.arul@claysys.info>
2023-08-29 15:02:30 +0530

    Comment changes

--- WebApiDemo/Services/UserService.cs
+++ WebApiDemo/Services/UserService.cs
@@ -68,7 +68,7 @@ namespace WebApiDemo.Services
             }
         }
 
-        /// <summary>
+        /// <summary>s
         /// Createing user
         /// </summary>
         /// <param name="userInput">Inserting user values as a object</param>
